0d72a5f59173d11856d0f13aa7cb2f9c36dfcf88
wandriey <jona434g@edu.easj.dk>
2018-11-09 23:49:45 +0100

    For loop removed
    Should be inclueded in the first place.

--- RockPaperScissorsConesoleApp/RockPaperScissorsConesoleApp/Program.cs
+++ RockPaperScissorsConesoleApp/RockPaperScissorsConesoleApp/Program.cs
@@ -6,12 +6,8 @@ namespace RockPaperScissorsConesoleApp
     {
         static void Main(string[] args)
         {
-            
-
-            for (int i = 0; i < 20; i++)
-            {
-                Fight fight = new Fight();
-            }
+            //Starts the application
+            Fight fight = new Fight();
         }
     }
 }
